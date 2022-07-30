b4e45e9c9b9460e373e85e7e52fd9d86a0600e0b
jatin <jatin.kshatriya2821@gmail.com>
2022-07-30 17:04:42 +0530

    merger

--- department/models.py
+++ department/models.py
@@ -29,7 +29,7 @@ class CustomAccountManager(BaseUserManager):
 
     def create_user(self, email, first_name, last_name, password, **other_fields):
         if not email:
-            raise ValueError(_('You must provide an email address'))
+            raise ValueError(('You must provide an email address'))
 
         email = self.normalize_email(email)
         user = self.model(email=email, first_name=first_name, last_name=last_name,**other_fields)
