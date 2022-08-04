4d6db0db9f7723e73db6006a76c8b8fec2876ff6
Praahant <76206077+Praahant@users.noreply.github.com>
2022-08-04 06:00:15 +0530

    commit

--- src/pages/Evaluation/Evalute.jsx
+++ src/pages/Evaluation/Evalute.jsx
@@ -2,45 +2,13 @@ import React from "react";
 import TextField from '@mui/material/TextField';
 function Evalute(props) {
   return (
-    
-      
-      <tr>
-
-      <td className="name">{props.name}</td>
-      <td className="enrollment">{props.enrollment}</td>
-      <td className="email">{props.email}</td>
-      <td className="mid-sem">
-      <TextField
-          id="standard-number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="standard"
-          />
-         </td>
-      <td className="mid-sem"><TextField
-          id="standard-number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="standard"
-          />
-         </td>
-      <td className="mid-sem"><TextField
-          id="standard-number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="standard"
-          />
-         </td>
-      
-   
-    </tr>
-    
+    <th className="TH"><div className="FirstAssignmentMarks">
+      <div>
+        <span>6aug</span>
+        <span></span>
+      </div>
+      <div></div>
+    </div></th>
   );
 }
 
