11a93703a5c18dfc481e39727435d5a167d8288d
Praahant <76206077+Praahant@users.noreply.github.com>
2022-08-11 21:01:51 +0530

    Assignment wala page

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -35,7 +35,7 @@ const Main = (props) => {
         },
         {
           title: 'Classwork',
-          path: `/classes`
+          path: `/classwork/:code`
         },
         {
           title: 'People',
